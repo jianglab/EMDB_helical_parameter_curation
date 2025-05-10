52c0328df72fbfcaad839113f081074727704f46
smallelephant9516 <lidaoyi16@hotmail.com>
2025-05-09 20:54:48 -0400

    update the hi3d dataframe bug

--- code/adjust_dataframe/hi3d_col.py
+++ code/adjust_dataframe/hi3d_col.py
@@ -12,8 +12,8 @@ print(df.columns)
 #https://helical-indexing-hi3d.streamlit.app/?emd_id=emd-14057&rise=2.368&twist=179.597&csym=1&rise2=2.367&twist2=-179.6&csym2=1
 
 df['HI3D_link'] = 'https://helical-indexing-hi3d.streamlit.app/?emd_id=emd-' + df['emdb_id'].str.extract(r'EMD-(\d+)', expand=False) \
-    + '&rise=' + df['rise_deposited (Å)'].astype(str) + '&twist=' + df['twist_deposited (°)'].astype(str) + '&csym=' + df['csym_deposited'].str[1:]\
-        + '&rise2=' + df['rise_validated (Å)'].astype(str) + '&twist2=' + df['twist_validated (°)'].astype(str) + '&csym2=' + df['csym_validated'].str[1:]
+    + '&rise=' + df['rise_validated (Å)'].astype(str) + '&twist=' + df['twist_validated (°)'].astype(str) + '&csym=' + df['csym_validated'].str[1:]\
+        + '&rise2=' + df['rise_deposited (Å)'].astype(str) + '&twist2=' + df['twist_deposited (°)'].astype(str) + '&csym2=' + df['csym_deposited'].str[1:]
 
 df2 = df.copy()
 
